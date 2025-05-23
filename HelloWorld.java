f00f325d06475a55103ead78fe02ba010ac757f3
Ganesh Gupta <ganeshgupta10043@gmail.com>
2025-05-23 09:21:51 +0530

    Merge remote changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello from DevOps Pipeline!");
+    public static void main(String[] args) throws InterruptedException {
+        while(true) {
+            System.out.println("Hello from DevOps Pipeline! - " + new java.util.Date());
+            Thread.sleep(5000); // Print every 5 seconds
+        }
     }
 }
